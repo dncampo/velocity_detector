11f13fe07210a1ada652bb4bcd204bdc0a9c3f93
chinomng <chinomng@gmail.com>
2016-03-25 01:03:22 +0000

    change tabs by spaces

--- velocity_04.py
+++ velocity_04.py
@@ -14,42 +14,41 @@ GPIO.setup(4,GPIO.IN) ## laser 1 al raspi gpio 4
 GPIO.setup(23,GPIO.IN)## laser 2 al raspi gpio 23
 
 def initialize_camera():
-	pygame.init()
-	pygame.camera.init()
-	camlist = pygame.camera.list_cameras()
-	if camlist:
-		cam = pygame.camera.Camera(camlist[0],(640,480))
-		cam.start()
-		return cam
+    pygame.init()
+    pygame.camera.init()
+    camlist = pygame.camera.list_cameras()
+    if camlist:
+        cam = pygame.camera.Camera(camlist[0],(640,480))
+        cam.start()
+        return cam
 
 def publish():
-	pass
+    pass
 
 def take_photo(cam):
-	image = cam.get_image()
-	pygame.image.save(image, datetime.datetime.now().isoformat() + ".jpg")
+    image = cam.get_image()
+    pygame.image.save(image, datetime.datetime.now().isoformat() + ".jpg")
 
 if __name__ == "__main__":
-	cam = initialize_camera()
-	while True:
-		while GPIO.input(4) == 0:
-			pass
+    cam = initialize_camera()
+    while True:
+        while GPIO.input(4) == 0:
+            pass
 		
-		print "paso sensor 1"
-		start_time = time.time()
-		time.sleep(0.57)
-		while GPIO.input(23) == 0:
-			pass
-		
-		end_time = time.time()
-		print "paso sensor 2"
+        print "paso sensor 1"
+        start_time = time.time()
+        time.sleep(0.57)
+        while GPIO.input(23) == 0:
+            pass
 
-		print end_time-start_time
-		delta_time = end_time - start_time
-		print delta_time, "delta_time"
-		speed = MS_2_KMH * (LASERS_DISTANCE / delta_time)
-		if speed > SPEED_LIMIT:
-			take_photo(cam)
-			publish()
-		print "SPEED = ", speed
+        end_time = time.time()
+        print "paso sensor 2"
 
+        print end_time-start_time
+        delta_time = end_time - start_time
+        print delta_time, "delta_time"
+        speed = MS_2_KMH * (LASERS_DISTANCE / delta_time)
+        if speed > SPEED_LIMIT:
+            take_photo(cam)
+            publish()
+        print "SPEED = ", speed
