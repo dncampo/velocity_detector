53fc5b6ec05bb1c48106d1b3506362e611a2d956
chinomng@gmail.com <mgalan@machinalis.com>
2016-04-15 12:17:42 -0300

    coding styles

--- opencv/car_counting/Blob.cpp
+++ opencv/car_counting/Blob.cpp
@@ -1,11 +1,8 @@
-// Blob.cpp
-
 #include "Blob.h"
 #include <iostream>
 using namespace std;
-///////////////////////////////////////////////////////////////////////////////////////////////////
-Blob::Blob(vector<cv::Point> _contour) {
 
+Blob::Blob(vector<cv::Point> _contour) {
     currentContour = _contour;
 
     currentBoundingRect = cv::boundingRect(currentContour);
@@ -28,10 +25,8 @@ Blob::Blob(vector<cv::Point> _contour) {
 
     ts[0] = ts[1] = 0;
     speed = 0.0;
-
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
 double Blob::getSpeed(float distance){
     if (speed != 0.0) return speed;
     long int delta = abs(ts[1] - ts[0]);
@@ -40,91 +35,56 @@ cout << "ts[0]: "  << ts[0] << "  ts[1]: " << ts[1] << endl;
     if (delta <= 0.0) return 0;
     double deltaH = delta / 1000.0;
     double distanceKM = distance / 1000.0;
-    speed = distanceKM/(deltaH/3600);
+    speed = distanceKM / (deltaH / 3600);
 cout << "speed: "  << speed << endl;
     return speed;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
 void Blob::predictNextPosition(void) {
-
     int numPositions = (int)centerPositions.size();
-
     if (numPositions == 1) {
-
         predictedNextPosition.x = centerPositions.back().x;
         predictedNextPosition.y = centerPositions.back().y;
-
-    }
-    else if (numPositions == 2) {
-
+    } else if (numPositions == 2) {
         int deltaX = centerPositions[1].x - centerPositions[0].x;
         int deltaY = centerPositions[1].y - centerPositions[0].y;
-
         predictedNextPosition.x = centerPositions.back().x + deltaX;
         predictedNextPosition.y = centerPositions.back().y + deltaY;
 
-    }
-    else if (numPositions == 3) {
-
+    } else if (numPositions == 3) {
         int sumOfXChanges = ((centerPositions[2].x - centerPositions[1].x) * 2) +
             ((centerPositions[1].x - centerPositions[0].x) * 1);
-
         int deltaX = (int)round((float)sumOfXChanges / 3.0);
-
         int sumOfYChanges = ((centerPositions[2].y - centerPositions[1].y) * 2) +
             ((centerPositions[1].y - centerPositions[0].y) * 1);
-
         int deltaY = (int)round((float)sumOfYChanges / 3.0);
-
         predictedNextPosition.x = centerPositions.back().x + deltaX;
         predictedNextPosition.y = centerPositions.back().y + deltaY;
-
-    }
-    else if (numPositions == 4) {
-
+    } else if (numPositions == 4) {
         int sumOfXChanges = ((centerPositions[3].x - centerPositions[2].x) * 3) +
             ((centerPositions[2].x - centerPositions[1].x) * 2) +
             ((centerPositions[1].x - centerPositions[0].x) * 1);
-
         int deltaX = (int)round((float)sumOfXChanges / 6.0);
-
         int sumOfYChanges = ((centerPositions[3].y - centerPositions[2].y) * 3) +
             ((centerPositions[2].y - centerPositions[1].y) * 2) +
             ((centerPositions[1].y - centerPositions[0].y) * 1);
-
         int deltaY = (int)round((float)sumOfYChanges / 6.0);
-
         predictedNextPosition.x = centerPositions.back().x + deltaX;
         predictedNextPosition.y = centerPositions.back().y + deltaY;
-
-    }
-    else if (numPositions >= 5) {
-
+    } else if (numPositions >= 5) {
         int sumOfXChanges = ((centerPositions[numPositions - 1].x - centerPositions[numPositions - 2].x) * 4) +
             ((centerPositions[numPositions - 2].x - centerPositions[numPositions - 3].x) * 3) +
             ((centerPositions[numPositions - 3].x - centerPositions[numPositions - 4].x) * 2) +
             ((centerPositions[numPositions - 4].x - centerPositions[numPositions - 5].x) * 1);
-
         int deltaX = (int)round((float)sumOfXChanges / 10.0);
-
         int sumOfYChanges = ((centerPositions[numPositions - 1].y - centerPositions[numPositions - 2].y) * 4) +
             ((centerPositions[numPositions - 2].y - centerPositions[numPositions - 3].y) * 3) +
             ((centerPositions[numPositions - 3].y - centerPositions[numPositions - 4].y) * 2) +
             ((centerPositions[numPositions - 4].y - centerPositions[numPositions - 5].y) * 1);
-
         int deltaY = (int)round((float)sumOfYChanges / 10.0);
-
         predictedNextPosition.x = centerPositions.back().x + deltaX;
         predictedNextPosition.y = centerPositions.back().y + deltaY;
-
-    }
-    else {
+    } else {
         // should never get here
     }
-
 }
-
-
-
-
