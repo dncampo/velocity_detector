ecc9a6b7b7126211978656b3e6ead633aa761cca
David N. Campo <dncampo@gmail.com>
2016-04-15 19:43:50 -0300

    compile with no debug symbols and show all Warning messages

--- opencv/car_counting/compile.sh
+++ opencv/car_counting/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Primero se compila la clase
-g++ -c -ggdb Blob.cpp
-g++ -c -ggdb -std=c++11 -g main.cpp
+g++ -Wall -c Blob.cpp
+g++ -Wall -c -std=c++11 main.cpp
 
 #luego se compila el main, y se lo enlaza al objeto anterior
-g++ -o main main.o Blob.o `pkg-config --libs --cflags opencv` -ldl
+g++ -o main main.o Blob.o `pkg-config --libs --cflags opencv`
 
