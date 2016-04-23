9ac9c86d3ef961b1e7cd18265f9cd45673bae654
David N. Campo <dncampo@gmail.com>
2016-04-23 18:30:56 -0300

    compile with optimization flags

--- opencv/car_counting/compile.sh
+++ opencv/car_counting/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+OPT=-Ofast
 # Primero se compila la clase
-g++ -Wall -c Blob.cpp
-g++ -Wall -c -std=c++11 main.cpp
+g++ -Wall $OPT -c Blob.cpp
+g++ -Wall $OPT -c -std=c++11 main.cpp
 
 #luego se compila el main, y se lo enlaza al objeto anterior
-g++ -o main main.o Blob.o `pkg-config --libs --cflags opencv`
+g++ $OPT -o main main.o Blob.o `pkg-config --libs --cflags opencv`
 
