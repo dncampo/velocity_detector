0e98c4e60ac0c8d6c97c01b016f05d88703790c6
David@ David's RaspberryPi3 <dncampo@gmail.com>
2017-08-05 00:22:17 -0300

    unifying compitle script to work in both platforms

--- opencv/car_counting/compile.sh
+++ opencv/car_counting/compile.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+
+compile_flags=""
+arch="$(uname -m)"
+echo "$arch";
+if [[ $arch == arm* ]] ; then
+	echo "Compiling in Raspberry/ARM"
+	compile_flags='-lopencv_highgui -lopencv_imgproc -lopencv_core'
+elif [[ "$arch" == x86_64* ]] ; then
+	echo "Compiling in GNU/Linux @X86_64"
+	compile_flags=`pkg-config --libs --cflags opencv`
+else
+	echo "Unknown architecture. Exiting"
+	exit 1
+fi
+
 OPT=-Ofast
 # Primero se compila la clase
 g++ -Wall $OPT -c Blob.cpp
 g++ -Wall $OPT -c -std=c++11 main.cpp
 
 #luego se compila el main, y se lo enlaza al objeto anterior
-g++ $OPT -o main main.o Blob.o `pkg-config --libs --cflags opencv`
+g++ $OPT -o main main.o Blob.o $compile_flags
 
